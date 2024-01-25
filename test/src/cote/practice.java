66f170333a2c0918312e061ec66c3eab64947b17
smj <amjey@naver.com>
2024-01-25 16:26:26 +0900

    설명추가

--- test/src/cote/practice.java
+++ test/src/cote/practice.java
@@ -76,33 +76,40 @@ public static void main(String[] args) {
 	//값이 큰 요소부터 차례로 1,2,3... 순위를 매겨 배열로 다시 출력
 	int[] emergency = {3, 76, 24};
 
-//	// 원래 배열 인덱스(위치)로 순위 매기기
-//    int[] ranks = IntStream.range(0, emergency.length)
-//            .boxed()
-//            .sorted((i, j) -> Integer.compare(emergency[j], emergency[i]))
-//            .mapToInt(i -> i + 1)
-//            .toArray();
-//
-//    System.out.println("순위 매기기: " + Arrays.toString(ranks));
-//
-//    // 배열 요소의 원래 위치 유지
-//    int[] result = IntStream.range(0, emergency.length)
-//            .boxed()
-//            .sorted((i, j) -> Integer.compare(ranks[i], ranks[j]))
-//            .mapToInt(i -> i+1)
-//            .toArray();
+	// emergency 배열 요소를 내림차순 정렬하여 인덱스로 표시
+    int[] ranks = IntStream.range(0, emergency.length)
+            .boxed()
+            .sorted((a, b) -> Integer.compare(emergency[b], emergency[a]))
+            .mapToInt(i -> i + 1)
+            .toArray();
 
+    System.out.println("내림차순 정렬 인덱스: " + Arrays.toString(ranks));
+
+    // 인덱스를 기준으로 원래 배열을 오름차 ==> 내림차 변경하므로 결국 배열의 요소가 원래 위치로 돌아오게 됨
+    // ranks 배열 요소를 오름차순 정렬하여 인덱스로 표시
+    int[] result = IntStream.range(0, emergency.length)
+            .boxed()
+            .sorted((a, b) -> Integer.compare(ranks[a], ranks[b]))
+            .mapToInt(i -> i+1)
+            .toArray();
+    
+    System.out.println(Arrays.toString(result));
+	
 	//희표좌ver. 초간단 코드
 	int[] answer = Arrays.stream(emergency)
 				.map(i -> Arrays.stream(emergency)
 										.boxed()	//각 요소를 Integer로 boxing
-										.sorted(Comparator.reverseOrder())	//내림차순 정렬
+										.sorted(Comparator.reverseOrder())	//내림차순 정렬 [76, 24, 3]
 										.collect(Collectors.toList())	//리스트로 수집
-										.indexOf(i)+1)	//현재 요소의 인덱스에 1을 더하기
-										.toArray();
+										.indexOf(i)+1)	//emergency[i]가 수집된 리스트에서 어느 위치(인덱스)에 있는지 찾아서 1을 더함
+																//emergency[0] = 2, emergency[1] = 0, emergency[2] = 1에 있음 ==> +1하면 [3, 1, 2]
+				.toArray();
 										
-		//*indexOf() : 주어진 괄호 안의 요소가 리스트/배열에서 처음으로 나타내는 인덱스를 찾아줌
-	System.out.println(Arrays.toString(answer));
+		// *indexOf() : 주어진 괄호 안의 요소가 리스트/배열에서 처음으로 나타내는 인덱스를 찾아줌
+		// 3은 정렬된 배열에서 3번째에 위치하므로 3
+		// 76은 정렬된 배열에서 1번째에 위치하므로 1
+		// 24는 정렬된 배열에서 2번째에 위치하므로 2
+	System.out.println("희표좌 답안: "+Arrays.toString(answer));
 	
 	
 	
